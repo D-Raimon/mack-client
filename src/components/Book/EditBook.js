6f2215b2fe78180402e872462eacc8854977d865
D-Raimon <raimon67@gmail.com>
2020-02-13 22:15:02 -0500

    Create new component to edit a Book
    - Created a new EditBook component to allow a user to edit a Book

---
+++ src/components/Book/EditBook.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+
+import apiUrl from '../../apiConfig'
+import Form from '../Form/Form'
+
+const EditBook = (props) => {
+  const [book, setBook] = useState({ title: '', picture: '', website: '', chapter: '', description: '' })
+  const [updated, setUpdated] = useState(false)
+
+  useEffect(() => {
+    axios(`${apiUrl}/books/${props.match.params.id}`)
+      .then(res => setBook(res.data.show))
+      .catch()
+  })
+
+  const handleChange = event => {
+    event.persist()
+    setBook(book => ({ ...book, [event.target.name]: event.target.value }))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    axios({
+      url: `${apiUrl}/books/${props.match.params.id}`,
+      method: 'PATCH',
+      data: { book },
+      headers: { 'Authorization': `Token token=${props.user.token}` }
+    })
+      .then(res => setUpdated(true))
+      .catch()
+  }
+
+  if (updated) {
+    return <Redirect to={`/books/${props.match.params.id}`} />
+  }
+
+  return (
+    <Form
+      book={book}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  )
+}
+
+export default EditBook
