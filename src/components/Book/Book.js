bdfd49fa45015f0e197ece34a5e32de5c4f36ed4
D-Raimon <raimon67@gmail.com>
2020-02-14 23:05:38 -0500

    Add delete to Book component
    - Added the functionality for a user to delete a Book to the Book.js
    component.

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -13,6 +13,14 @@ const Book = (props) => {
       .catch()
   })
 
+  const destroy = () => {
+    axios({
+      url: `${apiUrl}/books/${props.match.params.id}`,
+      method: 'DELETE',
+      headers: { 'Authorization': `Token token=${props.user.token}` }
+    })
+  }
+
   if (!book) {
     return <h1>Loading...</h1>
   }
@@ -30,6 +38,7 @@ const Book = (props) => {
       </div>
       <Link to={`/books/${props.match.params.id}/edit`}>
         <Button className="mt-1 mr-1 mb-3" size='sm' variant='danger'>Edit</Button>
+        <Button className="mt-1 mr-1 mb-3" size='sm' variant='danger' onClick={destroy}>Delete</Button>
       </Link>
     </Fragment>
   )
