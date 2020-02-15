584fdfb38af75d217866accd03a05a6e9bde6f5e
D-Raimon <raimon67@gmail.com>
2020-02-14 22:53:31 -0500

    Add Edit Button to Book component
    - Added a Edit button to the Book component so a user can now
    edit a Book entry.

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
-
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 import apiUrl from '../../apiConfig'
 
 const Book = (props) => {
@@ -27,6 +28,9 @@ const Book = (props) => {
           <li>Synopsis: {book.description}</li>
         </ul>
       </div>
+      <Link to={`/books/${props.match.params.id}/edit`}>
+        <Button className="mt-1 mr-1 mb-3" size='sm' variant='danger'>Edit</Button>
+      </Link>
     </Fragment>
   )
 }
