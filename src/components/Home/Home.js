b6525a12511cae486d97f12c6acf2b7e7617e517
D-Raimon <raimon67@gmail.com>
2020-02-17 23:17:33 -0500

    Create Home component for landing page
    - Created a home component for a landing page for the website so that
    when a user navigates to the site they won't just be greeted by a blank
    page.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Home = () => {
+  return (
+    <Fragment>
+      <h1>Welcome to Mack, the manga tracking app!</h1>
+    </Fragment>
+  )
+}
+
+export default Home
