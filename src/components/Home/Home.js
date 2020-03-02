04d4c547217ede88a5ea4eedab04676f204391d1
D-Raimon <raimon67@gmail.com>
2020-03-01 23:30:41 -0500

    Add background image for Home component
    - Added a background image for the home component and also made some
    additional styling adjustments

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './home.scss'
 const Home = () => {
   return (
     <Fragment>
-      <div className="background">
+      <div className="home_primary">
         <div className="welcome_message">Welcome to Mack, the manga tracking app!</div>
       </div>
     </Fragment>
