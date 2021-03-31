9268ebf5caebf157fdcae38c526e6ec761f72e41
D-Raimon <raimon67@gmail.com>
2021-03-31 17:41:12 -0400

    Update Header Component
    - Moved some of the links in the navbar to dropdown menus to make the
    header a bit cleaner overall.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#/">Home</Nav.Link>
-    <Nav.Link href="#books">My Stories</Nav.Link>
-    <Nav.Link href="#create-book">Add a Story</Nav.Link>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <NavDropdown title="Stories" id="collasible-nav-dropdown">
+      <NavDropdown.Item href="#books">My Stories</NavDropdown.Item>
+      <NavDropdown.Item href="#create-book">Add a Story</NavDropdown.Item>
+    </NavDropdown>
+    <NavDropdown title="Account" id="collasible-nav-dropdown">
+      <NavDropdown.Item href="#change-password">Change Password</NavDropdown.Item>
+      <NavDropdown.Item href="#sign-out">Sign Out</NavDropdown.Item>
+    </NavDropdown>
   </Fragment>
 )
 
@@ -26,7 +31,7 @@ const Header = ({ user }) => (
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+        { user && <span className="navbar-text mr-2">{user.email}</span>}
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
